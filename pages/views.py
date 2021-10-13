3fd7680e349dae5aca5dce0b2c014a8758d6c3b7
sel17 <selvin17paul@gmail.com>
2021-10-13 15:27:22 +1100

    completed featured and latest cars section

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from .models import *
+from .models import Team
+from products.models import Product
 
 # Create your views here.
 def home(request):
     teams = Team.objects.all()
+    featured_products = Product.objects.order_by('-created_date').filter(is_featured=True)
+    all_products = Product.objects.order_by('-created_date')
     data = {
         'teams': teams,
+        'featured_products': featured_products,
+        'all_products': all_products,
     }
     return render(request, 'pages/home.html', data)
 
