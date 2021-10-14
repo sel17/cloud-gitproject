aa4e5e173d8a36f1cf28c4e9806f43ed6f007518
sel17 <selvin17paul@gmail.com>
2021-10-15 01:06:59 +1100

    finished facebook and google login

--- accounts/views.py
+++ accounts/views.py
@@ -60,6 +60,5 @@ def dashboard(request):
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
-        messages.success(request, 'You are successfully logged out')
         return redirect('home')
     return redirect('home')
